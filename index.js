5fb621a50d043b9043388cf27a511c09f6740b9e
Achmad Naufal Prayitno <anaufal9999@gmail.com>
2021-06-10 17:31:34 +0200

    Update index.js: now able top handle graphql API and hide mongoose server URI

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
+
+const auth = require('./middleware/is-auth');
+const mongoose = require('mongoose');
+
+const MONGODB_URI =
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_NAME}.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`
+
+
 // TODO CREATE GRAPQLSCHEMA
 
 
@@ -27,7 +37,39 @@ app.use((req, res, next) => {
 app.use(auth);
 
 // TODO create graphql schema and resolver
-app.use('/graphql', graphqlHTTP({
-    schema: graphqlSchema,
+app.use('/graphql',
+    graphqlHTTP({
+        schema: graphqlSchema,
+        rootValue: graphqlResolver,
+        graphiql: true,
+        customFormatErrorFn(err) {
+            if (!err.originalError) {
+                return err;
+            }
+            const data = err.originalError.data;
+            const message = err.message || 'An error occured!';
+            const code = err.originalError.code || 500;
+            return { message: message, status: code, data: data };
+        }
+    })
+);
 
-}))
+app.use((error, req, res, next) => {
+    const status = error.statusCode || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({ message: message, data: data })
+});
+
+mongoose
+    .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => {
+        // for HTTPS: USE THIS
+        //   https.createServer({ key: privateKey, cert: certificate }, app)
+        //   .listen(process.env.PORT || 3000);
+        // })
+        app.listen(process.env.PORT || 3000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
